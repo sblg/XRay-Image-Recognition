4c09ca2cd2acf4877f423938fad872d7252030d8
sblg <39830895+sblg@users.noreply.github.com>
2023-08-03 09:36:00 +0200

    Create model_architecture.py

---
+++ models/model_architecture.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+from tensorflow.keras import layers
+
+def create_model(input_shape, num_classes):
+    model = tf.keras.Sequential([
+        layers.Conv2D(32, (3, 3), activation='relu', input_shape=input_shape),
+        layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+        layers.Conv2D(128, (3, 3), activation='relu'),
+        layers.MaxPooling2D((2, 2)),
+        layers.Flatten(),
+        layers.Dense(64, activation='relu'),
+        layers.Dense(num_classes, activation='softmax')
+    ])
+
+    return model
